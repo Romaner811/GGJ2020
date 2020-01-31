0fbb2bb22da17a33947046650cb62312fa74385f
Romaner811 <Romaner811@Gmail.com>
2020-01-31 18:23:05 +0200

    bugfix using width as height @animation.py

--- repairship/graphics/animation.py
+++ repairship/graphics/animation.py
@@ -2,29 +2,32 @@ from repairship.graphics import graphics_utils
 
 
 class Animation:
-    def __init__(self, sheet, frame_width, speed):
+    def __init__(self, sheet, size, speed):
         """
 
         :type sheet: pygame.Surface
-        :param frame_width:
+        :param size:
         """
         self.sheet = sheet
-        self.frame_width = frame_width
+        self.size = size
         self.speed = speed
 
-        self.frames_amount = int(self.sheet.get_width() / self.frame_width)
+        self.frames_amount = int(self.sheet.get_width() / self.size.width)
 
         self.frames = self.init_fames()
 
     def init_fames(self):
         frames = list()
-        frames_rects = graphics_utils.slice_animation(self.frame_width, self.frame_width, self.frames_amount)
+        frames_rects = graphics_utils.slice_animation(self.size.width, self.size.height, self.frames_amount)
 
         for rect in frames_rects:
             frame = graphics_utils.image_at(self.sheet, rect)
             frames.append(frame)
 
         return frames
+
+    def get_frame(self, idx):
+        return self.frames[idx]
 
     def get_frame_idx_at(self, time):
         return int(time / self.speed) % self.frames_amount
